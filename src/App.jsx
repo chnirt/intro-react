729947c12ec79b214f9c4c528260a8639fe83efe
Chnirt <35022967+chnirt@users.noreply.github.com>
2020-10-16 17:27:59 +0700

    create addStudent method

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,9 @@ class App extends React.Component {
   }
 
   /*Write an addStudent method here*/
+  addStudent(studentName) {
+    this.setState({ student: [students, studentName] });
+  }
 
   addGrade(assignment, student, score) {
     let grades = this.state.grades;
